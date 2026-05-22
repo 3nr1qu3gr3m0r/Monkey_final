ba455de6665ef5a11a0da9cc4f06f4007a026493
3nr1qu3gr3m0r <urienry.mhevf@gmail.com>
2026-05-21 22:50:25 -0600

    Vectorizacion de la base de datos

--- ia/vectorizador_local.py
+++ ia/vectorizador_local.py
@@ -21,6 +21,7 @@ def normalizar_texto(texto: str) -> str:
 def get_db_connection():
     return mysql.connector.connect(
         host=os.getenv("DB_HOST", "localhost"),
+        port=int(os.getenv("DB_PORT", 3306)),  # <--- ESTA ES LA LÍNEA MÁGICA
         user=os.getenv("DB_USER", "root"),
         password=os.getenv("DB_PASSWORD", ""),
         database=os.getenv("DB_NAME", "monkey_market")
